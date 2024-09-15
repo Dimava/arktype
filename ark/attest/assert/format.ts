7d7a9068b8df0ab93ef071ac45d97b1338860380
Dimava <dimava2@ya.ru>
2024-09-15 22:08:55 +0300

    use synckit instead of @prettier/sync

--- ark/attest/assert/format.ts
+++ ark/attest/assert/format.ts
@@ -1,76 +1,20 @@
-import { existsSync, readFileSync, writeFileSync } from "fs"
-import { join } from "path"
-import { format } from "prettier"
+import type { format } from "prettier"
+import { createSyncFn } from "synckit"
 import { getConfig } from "../config.ts"
 
-const prettier =
-	typeof Bun === "undefined" ? await import("@prettier/sync") : undefined
+const formatSync = createSyncFn<typeof format>(
+	require.resolve("./formatWorker.js")
+)
 
 const declarationPrefix = "type T = "
 
-const formatCachePath = join(getConfig().cacheDir, "..", "formatCache.json")
-const formatCache: Record<string, string | null> = JSON.parse(
-	existsSync(formatCachePath) ?
-		(readFileSync(formatCachePath, "utf8") ?? "{}")
-	:	"{}"
-)
-
-export const formatTypeString = (typeString: string): string => {
-	if (!prettier) {
-		if (formatCache[typeString]) return formatCache[typeString]
-		addFormat(typeString)
-		return '(not formatter yet) ' + typeString.trim()
-	}
-
-	return prettier
-		.format(`${declarationPrefix}${typeString}`, {
-			semi: false,
-			printWidth: 60,
-			trailingComma: "none",
-			parser: "typescript",
-			...getConfig().typeToStringFormat
-		})
+export const formatTypeString = (typeString: string): string =>
+	formatSync(`${declarationPrefix}${typeString}`, {
+		semi: false,
+		printWidth: 60,
+		trailingComma: "none",
+		parser: "typescript",
+		...getConfig().typeToStringFormat
+	})
 		.slice(declarationPrefix.length)
 		.trimEnd()
-}
-
-const addFormat = (typeString: string) => {
-	formatCache[typeString] = null
-	writeFileSync(formatCachePath, JSON.stringify(formatCache, null, 2))
-}
-
-await Promise.all(
-	Object.entries(formatCache).map(async ([typeString, formatted]) => {
-		// if (formatted) return
-		formatted = await format(`${declarationPrefix}${typeString}`, {
-			semi: false,
-			printWidth: 60,
-			trailingComma: "none",
-			parser: "typescript",
-			...getConfig().typeToStringFormat,
-			...{
-				useTabs: true,
-			}
-		})
-		formatted = formatted.slice(declarationPrefix.length).trimEnd()
-		formatCache[typeString] = formatted
-	})
-)
-writeFileSync(formatCachePath, JSON.stringify(formatCache, null, 2))
-
-// const addFormat = async (typeString: string) => {
-// 	console.log('start', typeString)
-// 	formatCache[typeString] = null
-// 	writeFileSync(formatCachePath, JSON.stringify(formatCache))
-// 	const formatted = await format(`${declarationPrefix}${typeString}`, {
-// 		semi: false,
-// 		printWidth: 60,
-// 		trailingComma: "none",
-// 		parser: "typescript",
-// 		...getConfig().typeToStringFormat
-// 	})
-// 	const result = formatted.slice(declarationPrefix.length).trimEnd()
-// 	console.log('done', result)
-// 	formatCache[typeString] = result
-// 	writeFileSync(formatCachePath, JSON.stringify(formatCache))
-// }
